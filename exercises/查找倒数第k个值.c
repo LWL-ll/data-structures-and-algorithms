40e295e4b42e9ca7dcf58476ac89b9398ad08068
LWL-ll <25071020216@stu.nsu.edu.cn>
2026-01-22 21:24:33 +0800

    重构练习代码结构并增强链表查找鲁棒性
    将“查找倒数第k个值.c”移动至 exercises 文件夹，并新增“查找两个单链表的第一个公共节点.c”文件及相关项目引用。优化 findNodeFs 函数，增加链表为空和 k 超界的判断，提升健壮性。新文件添加 _CRT_SECURE_NO_WARNINGS 以屏蔽安全警告。

--- exercises/查找倒数第k个值.c
+++ exercises/查找倒数第k个值.c
@@ -6,10 +6,19 @@ typedef struct Node{
 	struct Node* link;
 }Node;
 int findNodeFs(Node* L,int k) {
+	if (L->link == NULL) {
+		printf("链表为空！\n");
+		return -1;
+	}
 	Node* fast = L->link;
 	Node* slow = L->link;
-	for (int i = 1; i <= k; i++)
+	for (int i = 1; i <= k; i++) {
+		if (fast == NULL) {
+			printf("k值超过链表长度！\n");
+			return -1;
+		}
 		fast = fast->link;
+	}
 	while (fast != NULL) {
 		fast = fast->link;
 		slow = slow->link;
